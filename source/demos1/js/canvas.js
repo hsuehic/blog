2244d3a069682ffb432bda5172992b25d3a98b37
兴斋 <xingzhai.xxw@alibaba-inc.com>
2017-11-07 17:06:47 +0800

    post: regexpress implement Number.prototype.toLocaleString

--- source/demos1/js/canvas.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright(c) Alibaba Group Holding Limited.
- *
- * Authors:
- *   兴斋 <xingzhai.xxw@alibaba-inc.com> (https://www.gistop.com/)
- */
-window.onload = function () {
-  console.log('test');
-}
